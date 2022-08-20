eaea0f6dc398f882aaa295accf4a3a64d22b57ce
Oliver-Turp <tcgamingbusiness@gmail.com>
2022-08-20 14:13:35 +0100

    PM Index Done Bar Menu System

--- src/Phoenix_Media/Components/PM__Nav.jsx
+++ src/Phoenix_Media/Components/PM__Nav.jsx
@@ -3,42 +3,42 @@ import { NavLink, Link, Outlet } from "react-router-dom";
 import "../Styles/pm__nav.css";
 
 const PM__Nav = () => {
-  const container = document.querySelector(".pm__container");
+  // const container = document.querySelector(".pm__container");
 
-  document
-    .querySelector(".pm__header-wrap_hamburger")
-    .addEventListener("click", () => {
-      container.classList.add("change");
-    });
+  // document
+  //   .querySelector(".pm__header-wrap_hamburger")
+  //   .addEventListener("click", () => {
+  //     container.classList.add("change");
+  //   });
 
-  document
-    .querySelector(".pm__header-wrap_nav-close")
-    .addEventListener("click", () => {
-      container.classList.remove("change");
-    });
+  // document
+  //   .querySelector(".pm__header-wrap_nav-close")
+  //   .addEventListener("click", () => {
+  //     container.classList.remove("change");
+  //   });
 
-  const colors = [
-    "#03c04a",
-    "#f08080",
-    "#7289da",
-    "#f96854",
-    "#afeeee",
-    "#800080",
-  ];
+  // const colors = [
+  //   "#03c04a",
+  //   "#f08080",
+  //   "#7289da",
+  //   "#f96854",
+  //   "#afeeee",
+  //   "#800080",
+  // ];
 
-  let i = 0;
+  // let i = 0;
 
-  Array.from(document.querySelectorAll(".pm__nav-link")).forEach((item) => {
-    item.style.cssText = `background-color: ${colors[i++]}`;
-  });
+  // Array.from(document.querySelectorAll(".pm__nav-link")).forEach((item) => {
+  //   item.style.cssText = `background-color: ${colors[i++]}`;
+  // });
 
-  Array.from(document.querySelectorAll(".navigation-button")).forEach(
-    (item) => {
-      item.onclick = () => {
-        item.parentElement.parentElement.classList.toggle("change");
-      };
-    }
-  );
+  // Array.from(document.querySelectorAll(".navigation-button")).forEach(
+  //   (item) => {
+  //     item.onclick = () => {
+  //       item.parentElement.parentElement.classList.toggle("change");
+  //     };
+  //   }
+  // );
 
   return (
     <>
@@ -99,7 +99,60 @@ const PM__Nav = () => {
         </div>
 
         <Outlet />
-        <div className="pm__footer-wrap"></div>
+        <div className="pm__footer-wrap">
+          <div className="pm__footer-heading">
+            <h2>useful links</h2>
+          </div>
+          <div className="pm__footer-links">
+            <Link to="/">top comments</Link>
+            <Link to="/">wall of fame</Link>
+            <Link to="/">friends of phoenix</Link>
+            <Link to="/">random</Link>
+            <Link to="/" id="pm__footer-links_double">
+              TurpoChargedGaming
+            </Link>
+          </div>
+          <div className="pm__footer-heading">
+            <h2>newsletter</h2>
+          </div>
+          <form action="" method="post">
+            <input
+              type="text"
+              name="news-name"
+              placeholder="Your Name"
+              className="pm__footer-form_name"
+              required
+            />
+            <input
+              type="email"
+              name="news-email"
+              placeholder="Your_Email@Domain.com"
+              className="pm__footer-form_email"
+              required
+            />
+            <input
+              type="submit"
+              value="Send"
+              name="news-submit"
+              className="pm__footer-form_btn"
+            />
+            <div className="pm__footer-copyRight">
+              <p>
+                copyright &copy;2020-2022 phoenix media group. all rights
+                reserved.
+              </p>
+            </div>
+          </form>
+          <div className="pm__footer-heading">
+            <h2>keep in touch</h2>
+          </div>
+          <div className="pm__footer-media">
+            <Link to="/">facebook</Link>
+            <Link to="/">twitter</Link>
+            <Link to="/">instagram</Link>
+            <Link to="/">discord</Link>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/Phoenix_Media/Pages/PM__Home.js
+++ src/Phoenix_Media/Pages/PM__Home.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import Balloon from "../Assets/Images/air-balloon.png";
+
+import "../Styles/pm__home.css";
 
 const PM__Home = () => {
   return (
-    <div>PM__Home</div>
-  )
-}
+    <div className="pm__home-wrap">
+      <div className="pm__home-txt">
+        <h1>
+          welcome to <br />
+          the family
+        </h1>
+        <p></p>
+      </div>
 
-export default PM__Home
+      <img src={Balloon} alt="air ballon" />
+    </div>
+  );
+};
+
+export default PM__Home;
